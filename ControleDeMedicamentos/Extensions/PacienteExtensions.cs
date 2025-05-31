0c85bcff6d26b8e5dcb3c67b147cd72c29753b09
Iagodizlla <iagoschlemper@gmail.com>
2025-05-31 18:05:08 -0300

    Implementado pagina de paciente

---
+++ ControleDeMedicamentos/Extensions/PacienteExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ControleDeMedicamentos.Model;
+using ControleDeMedicamentos.ModuloPaciente;
+
+namespace ControleDeMedicamentos.Extensions;
+
+public static class PacienteExtensions
+{
+    public static Paciente ParaEntidade(this FormularioPacienteViewModel formularioVM)
+    {
+        return new Paciente(formularioVM.Nome, formularioVM.Telefone, formularioVM.CartaoSus);
+    }
+
+    public static DetalhesPacienteViewModel ParaDetalhesVM(this Paciente paciente)
+    {
+        return new DetalhesPacienteViewModel(
+            paciente.Id,
+            paciente.Nome,
+            paciente.Telefone,
+            paciente.CartaoSUS
+        );
+    }
+}
